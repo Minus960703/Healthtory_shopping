0fc0587bc90afa61ec965c5e394223fcceb674d0
Minus960703 <55120608+Minus960703@users.noreply.github.com>
2021-12-12 23:39:59 +0900

    main_sunday2

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,31 +1,31 @@
 $(function(){
-    $('.main_banner_menubar').hover(()=>{    
-        $('.menu_img').removeClass('active');
-        $('.menu_img_hover').addClass('active');
-    },
-    ()=>{
-        $('.menu_img').addClass('active');
-        $('.menu_img_hover').removeClass('active');
-    }); 
+    $('.main_banner_menubar').hover(()=>{
+            $('.menu_img').removeClass('active');
+            $('.menu_img_hover').addClass('active');
+        },
+        ()=>{
+            $('.menu_img').addClass('active');
+            $('.menu_img_hover').removeClass('active');
+        });
     var lastScrollTop = 0;
     $(window).scroll(()=>{
         var scrollTop = $(this).scrollTop();    //스크롤바 수직 위치 가져옴.        
         if(scrollTop > 0){
             if((scrollTop > lastScrollTop) ** (lastScrollTop > 0)){
-                $('.main_banner_menubar').css("top","-80px");                               
-            }                   
+                $('.main_banner_menubar').css("top","-80px");
+            }
             else {
                 $('.main_banner_menubar').css("top","0px");
-                $('.main_banner_menubar').addClass('drop');   
+                $('.main_banner_menubar').addClass('drop');
                 $('.menu_img').removeClass('active');
-                $('.menu_img_hover').addClass('active');             
+                $('.menu_img_hover').addClass('active');
             }
             lastScrollTop = scrollTop;
-        }        
+        }
         else {
             $('.main_banner_menubar').removeClass('drop');
             $('.menu_img').addClass('active');
             $('.menu_img_hover').removeClass('active');
         }
-    })    
+    })
 })
