7e67c5d591dfc22ba828e1b8bed2fa63b9350448
Minus960703 <55120608+Minus960703@users.noreply.github.com>
2022-01-12 22:51:01 +0900

    commit_cjw

---
+++ src/main/webapp/resources/js/introduce.js
@@ -0,0 +1,49 @@
+$(function(){
+    $('.main_banner_menubar').hover(()=>{
+            $('.menu_img').removeClass('active');
+            $('.menu_img_hover').addClass('active');
+        },
+        ()=>{
+            $('.menu_img').addClass('active');
+            $('.menu_img_hover').removeClass('active');
+        });
+    var lastScrollTop = 0;
+    $(window).scroll(()=>{
+        var scrollTop = $(this).scrollTop();    //스크롤바 수직 위치 가져옴.
+        if(scrollTop > 0){
+            if((scrollTop > lastScrollTop) ** (lastScrollTop > 0)){
+                $('.main_banner_menubar').css("top","-80px");
+            }
+            else {
+                $('.main_banner_menubar').css("top","0px");
+                $('.main_banner_menubar').addClass('drop');
+                $('.menu_img').removeClass('active');
+                $('.menu_img_hover').addClass('active');
+            }
+            lastScrollTop = scrollTop;
+        }
+        else {
+            $('.main_banner_menubar').removeClass('drop');
+            $('.menu_img').addClass('active');
+            $('.menu_img_hover').removeClass('active');
+        }
+    })
+    $('.main_container_menu ul li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+        $('.main_container_menu ul li').removeClass('active');
+        $(this).addClass('active');
+        $('.main_container_content').removeClass('active');
+
+        $("#"+tab_id).addClass('active');
+    })
+    $('.trainer_info').click(function() {
+        var tab_id = $(this).attr('data-tab');
+        $('.trainer_info_content').removeClass('active');
+
+        $("#"+tab_id).addClass('active');
+    })
+    $('.header_menu li').click(function() {
+        var click_id = $(this).attr('data-id')
+        location.href=click_id+'.html'
+    })
+})
